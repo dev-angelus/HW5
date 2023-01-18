cac06f85e616f2e895cd6a1b1995038edf9486c3
dev-angelus <abdillaeva.n@gmail.com>
2023-01-18 17:55:13 +0600

    HW5

---
+++ class.py
@@ -0,0 +1,26 @@
+
+class Hero:
+    def __init__(self, name, ability):
+        self.name = name
+        self.ability = ability
+
+    def __str__(self):
+        return f'Name of hero - {self.name}\n'\
+              f'Ability - {self.ability}'
+
+class Hero_super(Hero):
+
+    def __init__(self, name, ability):
+        super().__init__(name, ability)
+
+    def __str__(self):
+        return f'{self.name} {self.ability}'
+
+    def itis(self):
+        return f'{self.name} - it is super_hero'
+
+s1 = Hero('Doctor Strange', 'can create complex shields and barriers')
+s = Hero_super('Spiderman','can fly using spy web')
+
+print(f'Маг метод str: {s} ')
+print(f'Метод который выводит имя героя и надпись it is super_hero: , {s.itis()}')
---
+++ init.py
@@ -0,0 +1,14 @@
+from hw5 import Hero_super
+from hw5 import Hero
+
+s2 = Hero_super('Captain America', 'master tactician and field commander')
+
+s3 = Hero('Batman', 'Superhuman')
+
+print(f'\nМаг метод str: {s2} ')
+print(f'Метод который выводит имя героя и надпись it is super_hero: , {s2.itis()}')
+
+
+print(f'\n{s3} ')
+
+
