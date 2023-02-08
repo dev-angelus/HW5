97671a0130c558ac014bffea0127d6c16beea83e
dev-angelus <abdillaeva.n@gmail.com>
2023-02-08 20:25:08 +0600

    hw1.2

--- init.py
+++ init.py
@@ -5,10 +5,9 @@ s2 = Hero_super('Captain America', 'master tactician and field commander')
 
 s3 = Hero('Batman', 'Superhuman')
 
-print(f'\nМаг метод str: {s2} ')
+print(f'\nМаг метод str: {s2.itis()} ')
 print(f'Метод который выводит имя героя и надпись it is super_hero: , {s2.itis()}')
 
 
 print(f'\n{s3} ')
-
 
